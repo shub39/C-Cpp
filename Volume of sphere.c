9507b641080e7e44154fb76a11c5460272edfc1e
shub39 <cptnshubham39@gmail.com>
2023-09-29 17:52:46 +0530

    silly mistake

--- Volume of sphere.c
+++ Volume of sphere.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main(){
-    float radius = 10;
+    float radius;
+    printf("Type a Number : \n");
+    scanf("%f", &radius);
     float pi = 3.14159;
     float volume = 4/3*pi*radius*radius*radius;
-    printf ("The Volume is %f",volume);
+    printf ("The Volume is : %f", volume);
     return 0;
 }
 // continue from 2:04:00
