4f75136f8b5d8a23355908336d75abe51cb339dd
shub39 <cptnshubham39@gmail.com>
2023-09-29 19:44:50 +0530

    Profit or loss calculator

---
+++ ProfitLoss.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    float cp;
+    float sp;
+    printf("Enter Cost Price :");
+    scanf("%f",&cp);
+    printf("Enter Selling Price :");
+    scanf("%f",&sp);
+    if (cp<sp){
+        printf("Profit of %f",sp-cp);
+    }
+    if (cp>sp){
+        printf("Loss of %f",cp-sp);
+    }
+    if (cp==sp){
+        printf("No Profit Or Loss");
+    }
+    return 0;
+}
