ceff20604e050b807ce200f4ae2c1bf256a31d88
Shubham <cptnshubham39@gmail.com>
2024-04-09 08:35:37 +0530

    OOPS

--- (C++)OOP/intro.cpp
+++ (C++)OOP/intro.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
 
 using namespace std; 
+
 class Hero {
+private:
+  int health;
+  char level;
+public:
+  void setHealth(int n) {
+    health = n;
+  }
+  void setLevel(char n) {
+    level = n;
+  }
+  int getHealth() {
+    return health;
+  }
+  char getLevel() {
+    return level;
+  }
 };
 
 int main() {
+  Hero luffy;
+  luffy.setHealth(100);
+  luffy.setLevel('A');
+  cout << luffy.getHealth() << endl;
+  cout << luffy.getLevel() << endl;
+
+  Hero *zoro = new Hero;
+  zoro->setHealth(150);
+  zoro->setLevel('O');
+  cout << zoro->getHealth() << endl;
+  cout << zoro->getLevel() << endl;
 }
