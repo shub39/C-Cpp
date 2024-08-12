ba654c8b10f0aa568e4bccdf871460b95f51bf55
shub39 <shub39@pad39.lan>
2024-08-12 17:54:26 +0530

    More Restructuring 📦️

---
+++ DSA-3rd-Sem/Assignment-1/1.c
@@ -0,0 +1,19 @@
+/*
+ * Once upon a time, in the kingdom of Numeria, there was a legend about three
+hidden treasures that could make one the most powerful ruler of the land. These
+treasures were unique and distinct, each guarded by a different dragon. The
+king, eager to secure his legacy, sent out his bravest knight, Sir Num, on a
+quest to find these treasures. Sir Num embarked on his journey, collecting
+valuable gems along the way. Each gem had a magical number associated with it.
+Sir Num’s challenge was to identify the gems that represented the top three
+distinct maximum numbers, in order to unlock the treasure chests. If there were
+fewer than three distinct numbers, he could only unlock as many chests as there
+were distinct numbers. Can you help Sir Num find the third greatest treasure? If
+not, return the highest treasure he can find.
+*/
+
+#include <stdio.h>
+
+int main() {
+  int arr[3];
+}
--- Learning-C++/2dArrayOnHeap.cpp
+++ Learning-C++/2dArrayOnHeap.cpp
@@ -1,31 +1,34 @@
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
   int row;
   cin >> row;
-  
+
   int col;
   cin >> col;
 
-  int **arr = new int * [row];
-  for (int i = 0; i < row; i++) arr[i] = new int[col];
+  int **arr = new int *[row];
+  for (int i = 0; i < row; i++)
+    arr[i] = new int[col];
 
   for (int i = 0; i < row; i++) {
-    for (int j = 0; j < col; j++) cin >> arr[i][j];
+    for (int j = 0; j < col; j++)
+      cin >> arr[i][j];
   }
 
   cout << endl;
   for (int i = 0; i < row; i++) {
-    for (int j = 0; j < col; j++) cout << arr[i][j] << " ";
+    for (int j = 0; j < col; j++)
+      cout << arr[i][j] << " ";
     cout << endl;
   }
 
-  for (int i = 0; i < row; i++) { //Releasing memeory
-    delete [] arr[i];
+  for (int i = 0; i < row; i++) { // Releasing memeory
+    delete[] arr[i];
   }
-  delete [] arr;
+  delete[] arr;
 
   return 0;
 }
